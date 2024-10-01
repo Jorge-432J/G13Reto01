88b8b0fd261a34e207502b549bff7759c4d07643
Jorge-432J <jorge.marin.talentotech@usa.edu.co>
2024-10-01 16:00:22 -0500

    Cambios de mi hoja de vida

--- scripts.js
+++ scripts.js
@@ -1,31 +1,8 @@
-         // Datos de ejemplo
-        const experiencias = [
-            { puesto: "Asesor", empresa: "Staffing", periodo: "2024 - Presente", descripcion: "Atencion al usuario." }
-        ];
-
-    const educacion =[
-        { titulo: "Tecnólogo en Análisis y Desarrollo de Sistemas de Información", institucion: "SENA:", año: "2021"}
-    ];  
-
-     // Funcion para añadir experiencias
-    function añadirExperiencias() {
-        const listaExperiencia = document.getElementById(`lista-experiencia`)
-        experiencias.forEach(exp =>{
-            const li = document.createElement(`li`);
-            li.innerHTML = `<strong>${exp.puesto}</strong> en ${exp.empresa}<br>
-                            ${exp.periodo}<br>
-                            ${exp.descripcion}`;
-            listaExperiencia.appendChild(li);
-        });
-    }   
-
-    // Función para añadir educacion
-    function añadirEducacion() {
-        const listaEducacion = document.getElementById(`lista-educacion`);
-        educacion.forEach(edu => {
-            const li= document.createDocumentFragment(`11`);
-            li.innerHTML = `<stromp>${edu.titulo}</stron><br>
-                        ${edu.institucion, $(edu.año}`;
-            listaEducacion.appendChild(li);
-        });                               
-                            
+function toggleDetails(id) {
+    var details = document.getElementById(id);
+    if (details.classList.contains('hidden')) {
+        details.classList.remove('hidden');
+    } else {
+        details.classList.add('hidden');
+    }
+}
